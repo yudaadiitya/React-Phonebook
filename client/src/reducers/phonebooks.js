3134869ca45ffbd79c5b633ac6d8dff132be8ffd
Yuda <yuda10aditya@gmail.com>
2020-12-16 14:31:08 +0700

    modified reducers

--- client/src/reducers/phonebooks.js
+++ client/src/reducers/phonebooks.js
@@ -17,12 +17,6 @@ const phonebooks = (state = [], action) => {
                 }
             ]
 
-        // case 'ADD_PHONEBOOK_SUCCESS':
-        //     return action.phonebooks.map((item) => {
-        //         item.sent = true;
-        //         return item
-        //     })
-
         case 'ADD_PHONEBOOK_FAILURE':
             return state.map((item) => {
                 if (item.id === action.id) {
@@ -60,31 +54,6 @@ const phonebooks = (state = [], action) => {
                 return item;
             });
 
-        case 'EDIT_ON':
-            return state.map(item => ({
-                ...item,
-                ...(item.id === action.id && { edit: true })
-            }))
-
-
-        case 'EDIT_OFF':
-            return state.map(item => ({
-                ...item,
-                ...(item.id === action.id && { edit: false })
-            }))
-
-        case "SEARCH_CONTACT":
-            return state.map((item) => ({
-                ...item,
-                search: (item.id.toLowerCase().includes(action.value) || item.name.toLowerCase().includes(action.value) || item.phone.includes(action.value))
-            }))
-
-        case "SEARCH_CONTACT_RESET":
-            return state.map((item) => ({
-                ...item,
-                search: true
-            }))
-
 
         case 'DELETE_PHONEBOOK':
             console.log(action.id)
@@ -93,7 +62,7 @@ const phonebooks = (state = [], action) => {
         case 'RESEND_PHONEBOOK_SUCCESS':
             return state.map((item) => {
                 if (item.id === action.id) {
-                    item.sent = false;
+                    item.sent = true;
                 }
                 return item
             })
